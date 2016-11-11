094088c5b24e71c8dac776f2e56d9167e36a4da3
kgsi <kuusai1998@gmail.com>
2016-11-11 09:04:49 +0900

    5min limiter完成

--- 5min-limiter/js/content.js
+++ 5min-limiter/js/content.js
@@ -4,37 +4,93 @@ style.type = 'text/css';
 style.href = chrome.extension.getURL('css/limiter.css');
 (document.head||document.documentElement).appendChild(style);
 
-// $('body').css('background', 'blue');
-// document.body.style.backgroundColor= 'red';
-
 
 window.onload = function() {
 	if (!document.createElement) return;
 
-	var timerElment = document.createElement('div');		// 新規に要素（タグ）を生成
-	var timerText = document.createTextNode('00:00:00');	// 生成する要素の値（文字列）
-	timerElment.appendChild(timerText);					// 生成する要素の作成（要素に値を追加）
-  timerElment.classList.add('limiter');
+	var timerElment = document.createElement('div');
+	var timerText   = document.createTextNode('00:00:00');
+	timerElment.appendChild(timerText);
+  timerElment.classList.add('limiter5min');
 
-	document.body.appendChild(timerElment);				// このページ (document.body) の最後に生成した要素を追加
+	var endElment = document.createElement('div');
+  endElment.classList.add('limiter5min-shutdown');
+
+	var endElmentInner = document.createElement('div');
+	endElmentInner.classList.add('limiter5min-shutdown-inner');
+
+	var endElmentBtn1    = document.createElement('div');
+	var endElmentBtn1Txt = document.createTextNode('CONTINUE');
+	endElmentBtn1.appendChild(endElmentBtn1Txt);
+  endElmentBtn1.classList.add('btn','btn-attention');
+
+	var endElmentBtn2    = document.createElement('div');
+	var endElmentBtn2Txt = document.createTextNode('END');
+	endElmentBtn2.appendChild(endElmentBtn2Txt);
+  endElmentBtn2.classList.add('btn','btn-primary');
+
+	endElment.appendChild(endElmentInner).appendChild(endElmentBtn1);
+	endElment.appendChild(endElmentInner).appendChild(endElmentBtn2);
+
+	document.body.appendChild(endElment);
+	document.body.appendChild(timerElment);
+
+	$('.limiter5min-shutdown').css('visibility', 'hidden');
+
+	$('.limiter5min-shutdown .btn-attention').on('click', function(){
+		playjiho();
+		startTimer();
+		$('.limiter5min-shutdown').css({'visibility':'hidden'}).animate({opacity: 0}, 500);
+		$('.limiter5min').css('display', 'block');
+	});
+
+	$('.limiter5min-shutdown .btn-primary').on('click', function(){
+		$('.limiter5min-shutdown').css({'visibility':'hidden'}).animate({opacity: 0}, 500);
+	});
 }
 // -->
 
 
+// YYYY/MM/DD hh:mm:ss
+
+function getNowDate(){
+	var dateObj = new Date();
+	dateObj.setMinutes(dateObj.getMinutes() + 5);
+
+	var year    = dateObj.getFullYear();
+	var month   = dateObj.getMonth()+1;
+	var day     = dateObj.getDate();
+	var hh      = dateObj.getHours();
+	var mm      = dateObj.getMinutes();
+	var ss      = dateObj.getSeconds();
+	var nowDate = year +'/'+ month +'/'+ day +' '+ hh +':'+ mm +':'+ ss;
+
+	return nowDate;
+}
+
 var soundJiho = chrome.extension.getURL('sound/jiho.mp3');
-
-createjs.Sound.registerSound(soundJiho, 'hoge');
-
-
-// setTimeout(function(){
-//   createjs.Sound.play('hoge');
-// }, 1000)
+var soundGong = chrome.extension.getURL('sound/gong.mp3');
+createjs.Sound.registerSound(soundJiho, 'jiho');
+createjs.Sound.registerSound(soundGong, 'gong');
 
 
 function playjiho(){
-  createjs.Sound.play('hoge');
+  createjs.Sound.play('jiho');
 }
 
+function playGong(){
+  createjs.Sound.play('gong');
+}
 
+function startTimer(){
+	var goalTIme = getNowDate();
 
-// createjs.Sound.registerSound("sound/jhou.mp3", soundID);
+	$('.limiter5min').countdown(goalTIme, function(event) {
+		$(this).html(event.strftime('%H:%M:%S'));
+
+	}).on('finish.countdown', function(){
+		playGong();
+		$('.limiter5min').css('display', 'none');
+		$('.limiter5min-shutdown').css({'visibility':'visible'}).animate({opacity: 1}, 500);
+	});
+}
--- 5min-limiter/js/popup.js
+++ 5min-limiter/js/popup.js
@@ -1,19 +1,24 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// chrome.tabs.executeScript(null, {file: 'libs/jquery.js'});
-// chrome.tabs.executeScript(null, {file: 'libs/createjs.js'});
+chrome.tabs.executeScript(null, {code:"$('.limiter5min').css('display', 'block');"});
+
+var clickedFlag = false;
 
 $(function(){
-
   // chrome.tabs.executeScript(null, {file: '5minlimiter.js'});
   // chrome.tabs.executeScript(null, {code:"$('.limiter').css('background', 'blue');"});
-  //
   // chrome.tabs.executeScript(null, {code:"$('body').append('<div>hello world</div>');"});
 
   $('.btn').on('click',function(){
-    chrome.tabs.executeScript(null, {code:"$('.limiter').css('background', 'blue');"});
-    chrome.tabs.executeScript(null, {code:"playjiho()"});
+
+    if(!clickedFlag) {
+      chrome.tabs.executeScript(null, {code:"$('.limiter5min').css('background', 'rgba(10, 10, 10, 0.90)');"});
+      chrome.tabs.executeScript(null, {code:"playjiho()"});
+      chrome.tabs.executeScript(null, {code:"startTimer()"});
+      $(this).text('NOW COUNT..');
+      $(this).addClass('disabled');
+      window.close();
+    }else {
+
+    }
   })
 });
 //
